a1cc7034cc53b752d0a61f57774e0b1356e76bd5
bitengineer09 <bitengineer09@gmail.com>
2025-10-25 07:42:26 +0530

    main.jsx changes

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import AuthContext from './store/AuthContext.jsx'
 import ProductContext from './store/ProductContext.jsx'
-import CartContext from '../../frontend/src/store/CartContext.jsx'
 import OrderContext from './store/OrderContext.jsx'
 
 createRoot(document.getElementById('root')).render(
